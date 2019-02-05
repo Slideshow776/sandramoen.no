bcefadd8ee784a341eff2e4f0960ddeea0bf219c
Sandra Moen <sandra77_6@hotmail.com>
2019-02-05 14:21:33 +0100

    Finished footer

--- source_code/src/components/Footer.vue
+++ source_code/src/components/Footer.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="footer">
-        <div id="company-info">
-            <p>Org nr: 133 7133 7133</p>
-            <p>Adresse: Anonsgate 25c, 0101, Oslo</p>
-        </div>
         <div id="contact">
-            <a id="mail" href="mailto:sandramoen01@gmail.com" target="_top">Send e-post</a>
-            <p>Telefon +47 47053281</p>
+            <ul>
+                <li><a id="contact" href="mailto:sandramoen01@gmail.com" target="_top">Send en e-post</a></li>
+                <li><a id="contact" href="tel:47053281">Telefon: +47 47053281</a></li>
+            </ul>
         </div>
         <div id="social-media">
-            <a href="" style="color:white"><i class="fa fa-facebook-square"></i></a>
-            <a href="" style="color:white"><i class="fa fa-youtube-square"></i></a>
+            <a href="https://www.facebook.com/sandra.doe.543" style="color:white"><i class="fa fa-facebook-square"></i></a>
+            <a href="https://www.youtube.com/channel/UCiWvUG5AeJ1lAkvlq0V-2cg?view_as=subscriber" style="color:white"><i class="fa fa-youtube-square"></i></a>
+            <a href="https://github.com/Slideshow776" style="color:white"><i class="fa fa-github"></i></a>
         </div>
     </div>
 </template>
@@ -20,6 +19,7 @@ export default {}
 </script>
 
 <style scoped>
+
 .footer {
     background-color: #c2c505;
     position: fixed;
@@ -27,30 +27,31 @@ export default {}
     bottom: 0;
     width: 100%;
     text-align: center;
-    color: white;
+    color: rgb(0, 0, 0);
     display: flex;
-    justify-content: space-around;
-    height:5rem;
+    justify-content: center;
+    height:3rem;
 }
 
-#mail {
-    color:white;
-}
-
-#company-info {
-    flex-direction: row;
-    margin-left:2rem;
-    padding:0rem;
-    text-align: left;
-}
-
-#social-media {
-    flex-direction: row;
-    margin-right:2rem;
+li {
+  list-style-type:none;
 }
 
 #contact {
-    flex-direction: row;
+    position:relative;
+    top:-.3rem;
+    color:rgb(0, 0, 0);
+    font-weight: 600;
+}
+
+#contact:hover {
+    color:#9211e8;
+}
+
+#social-media {
+    position:relative;
+    top:-.9rem;
+    margin-right:2rem;
 }
 
 .fa {
@@ -59,9 +60,13 @@ export default {}
   width: 3rem;
   text-align: center;
   text-decoration: none;
-  margin: 0rem .25rem;
   position: relative;
   top: .5rem;
+  color:rgba(0, 0, 0);
+}
+
+.fa:hover {
+    color:#9211e8;
 }
 
 </style>
